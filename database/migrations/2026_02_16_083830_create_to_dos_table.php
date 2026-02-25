0617acf9f78f159d4ed014a38f2721a1ccade7a0
Patriks Pužulis <puzulispatriks@gmail.com>
2026-02-25 11:22:22 +0200

    seeder

--- database/migrations/2026_02_16_083830_create_to_dos_table.php
+++ database/migrations/2026_02_16_083830_create_to_dos_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string("content");
             $table->boolean("completed");
+            $table->enum("priority", ["low", "medium", "high"]);
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\ToDo;
+use App\Models\Diary;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,10 +18,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        ToDo::factory()->count(10)->create();
+        Diary::factory()->count(15)->create();
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
